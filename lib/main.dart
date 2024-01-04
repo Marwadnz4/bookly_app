40f89f74872f9f9709a665e28911be51789c0d0b
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-04 16:30:05 +0300

    create splash screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bookly App',
+      showSemanticsDebugger: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
