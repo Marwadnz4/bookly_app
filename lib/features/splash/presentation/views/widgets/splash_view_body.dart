e871ad2d2b8d23568e8e9e6f29a29b036633e2e2
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-07 22:25:12 +0300

    add sliding animation to Splash View

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -16,6 +16,10 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
+  }
+
+  void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(minutes: 1),
@@ -24,6 +28,7 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
       begin: const Offset(0, 20),
       end: Offset.zero,
     ).animate(animationController);
+    animationController.forward();
   }
 
   @override
