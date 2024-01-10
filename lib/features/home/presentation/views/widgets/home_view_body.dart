893ab48edee79db4ffb00821b9b89e2282810e1f
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-10 19:45:07 +0300

    create styles and add title to home

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,23 +1,31 @@
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bare.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
-import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const CustomAppBar(),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * .3,
-          child: const FeaturedBooksListView(),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        children: [
+          const CustomAppBar(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: const FeaturedBooksListView(),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
