439e47107d4ab2bdc4c0f7751fb3372f324e2f3f
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-16 20:19:04 +0300

    create custom text field

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_body_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const String kHomeView = '/homeView';
   static const String kBookDetailsBodyView = '/bookDetailsBodyView';
+  static const String kSearchView = '/bookDetailsBodyView';
+
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +22,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsBodyView,
         builder: (context, state) => const BookDetailsBodyView(),
+      ),
+      GoRoute(
+        path: kBookDetailsBodyView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
