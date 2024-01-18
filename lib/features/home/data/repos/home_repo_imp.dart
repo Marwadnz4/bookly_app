11cb1348e17617fb0957aab8cbcd8b83f1d66d15
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-18 15:51:41 +0300

    create Server failure from dio error

--- lib/features/home/data/repos/home_repo_imp.dart
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -1,15 +1,33 @@
-import 'package:bookly_app/core/errors/failures.dart';
-
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-
+import 'dart:convert';
 import 'package:dartz/dartz.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dio/dio.dart';
 
 import 'home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (errMessage) {
+      if (errMessage is DioException) {
+        return left(ServerFailure.fromDioError(errMessage));
+      }
+      return left(ServerFailure(errMessage.toString()));
+    }
   }
 
   @override
